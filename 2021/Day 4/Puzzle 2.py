39b6b0e5f185c2999a96741f55c3611ce8e124e1
Jakub Wiśniewski <wisniaN7@gmail.com>
2022-10-25 17:58:41 +0200

    Day 4

--- 2021/Day 4/Puzzle 2.py
+++ 2021/Day 4/Puzzle 2.py
@@ -4,19 +4,21 @@ class Board:
             raise ValueError('Board must have 25 numbers')
         
         self.board = []
+        self.bingo = False
         
         for i in range(5):
             self.board.append(numbers[i * 5 : i * 5 + 5])
             
     def mark_number(self, number : int) -> int:
-        self.board = list(map(lambda row: list(map(lambda x: -1 if x == number else x, row)), self.board))
+        self.board = list(map(lambda row: list(map(lambda x: '-' if x == number else x, row)), self.board))
         
-        if self.check_bingo():
+        if not self.bingo and self.check_bingo():
+            self.bingo = True
             sum = 0
             
             for l in self.board:
                 for el in l:
-                    if el > 0:
+                    if type(el) is int:
                         sum += el
                         
             return sum * number
@@ -25,20 +27,26 @@ class Board:
 
     def check_bingo(self) -> bool:
         for i in range(5):
-            if self.board[i].count(-1) == 5:
+            if self.board[i].count('-') == 5:
                 return True
             
-            if list(map(lambda x: x[i], self.board)).count(-1) == 5:
+            if list(map(lambda x: x[i], self.board)).count('-') == 5:
                 return True
         
         return False
+        
+    def print(self) -> None:
+        for row in self.board:
+            print(row)
+        
+        print()
 
 numbersDrawn = []
 boards = []
 
 with open(r'2021\Day 4\input.txt') as f:
     numbersDrawn = f.readline().rstrip().split(',')
-
+    
     while board := f.read(76):
         l = []
         
@@ -47,10 +55,18 @@ with open(r'2021\Day 4\input.txt') as f:
             
         boards.append(Board(l))
         
-while num := numbersDrawn.pop(0):
-    for board in boards:
-        answer = board.mark_number(int(num))
+bingos = 0
         
-        if answer > 0:
-            print(answer)
-            exit()
+while num := numbersDrawn.pop(0):
+    for i, board in enumerate(boards):
+        if board:
+            answer = board.mark_number(int(num))
+            
+            if answer > 0:
+                bingos += 1
+                
+                if bingos == len(boards) - 1:
+                    print(answer)
+                    exit()
+                
+                boards[i] = None
